12757331fdb903a11d89b1332ed8e341ea1422d4
Daniel Mason <danmasson0@gmail.com>
2022-07-17 18:36:10 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.2da4978744a1c5fea919df7fcb0eea0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cffc4421b3f2b7b2df67dcce3a1ece",
+    "url": "/tabs-react-use-effect-practise/index.html"
+  },
+  {
+    "revision": "ba5ab3d6ca566b2422d9",
+    "url": "/tabs-react-use-effect-practise/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "cda7640575eb1ea898d7",
+    "url": "/tabs-react-use-effect-practise/static/js/2.4810a2d4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tabs-react-use-effect-practise/static/js/2.4810a2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5ab3d6ca566b2422d9",
+    "url": "/tabs-react-use-effect-practise/static/js/main.b78044ca.chunk.js"
+  },
+  {
+    "revision": "82586be0a2f63b01253a",
+    "url": "/tabs-react-use-effect-practise/static/js/runtime-main.443ab5c7.js"
+  }
+]);
